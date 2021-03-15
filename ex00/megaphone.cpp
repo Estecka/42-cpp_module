1b222f61f61363c472227e7ad2b42a4d1016fc34
abaur <abaur@student.42.fr>
2021-03-15 21:31:17 +0100

    Working megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,18 +6,29 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 16:47:38 by abaur             #+#    #+#             */
-/*   Updated: 2021/03/15 21:09:21 by abaur            ###   ########.fr       */
+/*   Updated: 2021/03/15 21:15:48 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+static void	toupper(char* s){
+	while (*s)
+	{
+		if ( 'a' <= *s && *s <= 'z')
+			*s += 'A' - 'a';
+		s++;
+	}
+}
+
 extern int	main(int argc, char** argv) {
 	if (argc <= 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else {
-		for (int i=1; i<argc; i++)
+		for (int i=1; i<argc; i++) {
+			toupper(argv[i]);
 			std::cout << argv[i];
+		}
 		std::cout << std::endl;
 	}
 }
