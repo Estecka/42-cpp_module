b434ac586f3de7f79fc78f70c415e76bbbdeebed
abaur <abaur@student.42.fr>
2021-03-16 22:05:07 +0100

    ex01 Tweaked some messages.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 23:04:13 by abaur             #+#    #+#             */
-/*   Updated: 2021/03/16 18:54:26 by abaur            ###   ########.fr       */
+/*   Updated: 2021/03/16 22:10:54 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,15 @@ extern int	main() {
 	std::string	input;
 
 	std::cout << "Welcome to your completely trusty and definitely not suspisci\
-ous phonebook" << std::endl;
+ous phonebook. It's swell to finally meet your other friends." << std::endl;
 	while (1) {
 		std::cout << "> ";
 		std::getline(std::cin, input);
 		if (input == "EXIT") {
-			std::cout << "Sayonara mf" << std::endl;
+			std::cout << "Thank you for using the trial version of Phonebook®" \
+				<< std::endl \
+				<< "Please buy a license in order to recover your saved datas."\
+				<< std::endl;
 			return EXIT_SUCCESS;
 		} 
 		else if (input == "ADD")
@@ -33,7 +36,10 @@ ous phonebook" << std::endl;
 			contact::Search();
 		else if (input == "add" || input == "search" || input == "exit")
 			std::cout << "Louder for those in the back." << std::endl;
+		else if (input == "")
+			;
 		else
-			std::cout << "Nani tf are you saying ?" << std::endl;
+			std::cout << "You speak a fascinating language, but alas, I only he\
+ar Latin." << std::endl;
 	}
 }
