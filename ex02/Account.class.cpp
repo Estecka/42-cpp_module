7b1818031de10065744ca6af8dc919ba27f987f4
abaur <abaur@student.42.fr>
2021-03-17 14:55:44 +0100

    ex02 Basic functionalities.

--- ex02/Account.class.cpp
+++ ex02/Account.class.cpp
@@ -6,11 +6,14 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 14:20:03 by abaur             #+#    #+#             */
-/*   Updated: 2021/03/17 14:46:47 by abaur            ###   ########.fr       */
+/*   Updated: 2021/03/17 14:54:16 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.class.hpp"
+
+#include <iostream>
+
 
 #pragma clang diagnostic ignored "-Wunknown-pragmas"
 
@@ -32,12 +35,17 @@ int	Account::getNbWithdrawals( void ) {
 }
 
 void	Account::displayAccountsInfos( void ) {
+	std::cout << "[Displaying accounts infos]";
 }
 #pragma endregion
 
 
 #pragma region PublicMember
 Account::Account( int initial_deposit ) {
+	this->_accountIndex = _nbAccounts++;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 1;
+	this->_nbWithdrawals = 0;
 }
 
 Account::~Account( void ) {
@@ -45,9 +53,19 @@ Account::~Account( void ) {
 
 
 void	Account::makeDeposit( int deposit ) {
+	this->_nbDeposits++;
+	this->_amount += deposit;
 }
 
 bool	Account::makeWithdrawal( int withdrawal ) {
+	if (withdrawal <= this->_amount)
+	{
+		this->_nbWithdrawals++;
+		this->_amount += withdrawal;
+		return true;
+	}
+	else
+		return false;
 }
 
 int		Account::checkAmount( void ) const {
@@ -66,11 +84,16 @@ int	Account::_totalNbDeposits;
 int	Account::_totalNbWithdrawals;
 
 void	Account::_displayTimestamp( void ) {
+	std::cout << "[19920104_091532] ";
 }
 #pragma endregion
 
 
 #pragma region PrivateMembers
 Account::Account( void ) {
+	this->_accountIndex = _nbAccounts++;
+	this->_amount = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
 }
 #pragma endregion
