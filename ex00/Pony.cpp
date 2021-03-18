6f53c2432c8e05a1d610150b89d084a269480198
abaur <abaur@student.42.fr>
2021-03-18 20:05:58 +0100

    ex00 Pony sleep and wake up.

--- ex00/Pony.cpp
+++ ex00/Pony.cpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 19:08:58 by abaur             #+#    #+#             */
-/*   Updated: 2021/03/18 19:27:58 by abaur            ###   ########.fr       */
+/*   Updated: 2021/03/18 19:56:56 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,40 @@ Pony::Pony(std::string name, std::string robeColor) {
 
 Pony::~Pony(){
 	std::cout << this->name << " was destroyed." << std::endl;
+}
+
+bool	Pony::Sleep(){
+	if (this->isAlive) {
+		std::cout << this->name << " went to sleep." << std::endl; \
+			;
+		if (this->isAwake)
+			std::cout << "Good dreams, " << this->name << "." << std::endl;
+		else
+			std::cout << "It's like a dream within a dream." << std::endl;
+		this->isAwake = false;
+		return true;
+	}
+	else {
+		std::cout << this->name << " is already sleeping, very, VERY deeply." \
+			<< std::endl;
+		return false;
+	}
+}
+
+bool	Pony::WakeUp(){
+	if (!this->isAlive) {
+		std::cout << "You attempt to wake " << this->name << " up, but they won\
+'t budge, whatever you do." << std::endl;
+		return false;
+	}
+	else if (this->isAwake) {
+		std::cout << "You shout at " << this->name << ", but they're already up\
+. They look at you with a concerned look on their horsy face." << std::endl;
+		return true;
+	}
+	else {
+		std::cout << "Rise and shine, " << this->name << ". rise, and shine..."\
+			<< std::endl;
+		return true;
+	}
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,22 +6,31 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 19:09:35 by abaur             #+#    #+#             */
-/*   Updated: 2021/03/18 19:32:33 by abaur            ###   ########.fr       */
+/*   Updated: 2021/03/18 20:03:19 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Pony.hpp"
 
+static void	PonyAdventuresTwoPointO(Pony *pony){
+	(void)pony;
+
+	pony->Sleep();
+	pony->Sleep();
+	pony->WakeUp();
+}
+
 static void	PonyOnTheHeap(){
 	Pony	*pony;
 
 	(void)pony;
+	// PonyAdventuresTwoPointO(pony);
 }
 
 static void	PonyOnTheStack(){
 	Pony	pony;
 
-	(void)pony;
+	PonyAdventuresTwoPointO(&pony);
 }
 
 extern int	main(){
