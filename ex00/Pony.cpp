0ebbcc36b7dc038ee4f153d6d5325782cb53f853
abaur <abaur@student.42.fr>
2021-03-18 22:10:21 +0100

    ex00 Run

--- ex00/Pony.cpp
+++ ex00/Pony.cpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 19:08:58 by abaur             #+#    #+#             */
-/*   Updated: 2021/03/18 21:27:53 by abaur            ###   ########.fr       */
+/*   Updated: 2021/03/18 22:09:32 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,21 +50,21 @@ bool	Pony::Sleep(){
 		return true;
 	}
 	else {
-		std::cout << name << " is already sleeping, very, VERY deeply." \
-			<< std::endl;
+		std::cout << name << " is sleeping, very, very deeply." << std::endl;
 		return false;
 	}
 }
 
 bool	Pony::WakeUp(){
 	if (!this->isAlive) {
-		std::cout << "You attempt to wake " << name << " up, but they won\
-'t budge, whatever you do." << std::endl;
+		std::cout << "You attempt to wake " << name << " up, "\
+			<< "but whatever you do, they won't budge." << std::endl;
 		return false;
 	}
 	else if (this->isAwake) {
-		std::cout << "You shout at " << name << ", but they're already up\
-. They look at you with a concerned look on their horsy face." << std::endl;
+		std::cout << "You shout at " << name << ", but they're already awake. "\
+			<< "They look at you with a concerned look on their horsy face." \
+			<< std::endl;
 		return true;
 	}
 	else {
@@ -78,8 +78,10 @@ bool	Pony::WakeUp(){
 void	Pony::BreakALeg(){
 	this->legsBroken++;
 	if (this->legsBroken >= 4){
-		std::cout << name << " broke all their legs. Unable to keep going, they\
- eventually succumbed to their wounds." << std::endl;
+		std::cout << name << " broke all their legs. Unable to keep going, "\
+			<< "they eventually succumbe to their wounds." << std::endl;
+		this->isAlive = false;
+		this->isAwake = false;
 	}
 }
 
@@ -90,17 +92,18 @@ bool	Pony::Jump() {
 		std::cout << name << " is unable to jump." << std::endl;
 		return false;
 	} else if (!this->isAwake) {
-		std::cout << name << " attempts to jump in their sleep. Doing so, they \
-break a leg." << std::endl;
+		std::cout << name << " attempts to jump in their sleep. Doing so, they"\
+			<< " break a leg." << std::endl;
 		this->BreakALeg();
 		return true;
 	} else {
 		if (this->legsBroken)
-			std::cout << name << " Clumsily attempts to jump." << std::endl;
+			std::cout << name << " clumsily attempts to jump." << std::endl;
 		else
 			std::cout << name << " performs a majestuous jump." << std::endl;
 		if (!(rand() % 4)) {
-			std::cout << "Unfortunately, they break a leg when landing." << std::endl;
+			std::cout << "Unfortunately, they break a leg as they land." \
+				<< std::endl;
 			this->BreakALeg();
 		}
 		return true;
@@ -114,21 +117,53 @@ bool	Pony::Walk() {
 		if (!this->isAwake){
 			std::cout << name << " starts sleepwalking." << std::endl;
 			if (!(rand() % 3)) {
-				std::cout << "Unable to see their surrounding, they twist a le\
-g on a pebble." << std::endl;
+				std::cout << "Unable to see their surrounding, they twist a le"\
+					<< "g on a pebble." << std::endl;
 				this->BreakALeg();
 			}
 		}
 		else if (this->legsBroken) {
-			std::cout << name << " limps with difficulty toward you." << std::endl;
+			std::cout << name << " limps with difficulty toward you." \
+				<< std::endl;
 			if (!(rand() % 4)) {
-				std::cout << "So much difficulty in fact, that they twist anoth\
-er leg." << std::endl;
+				std::cout << "So much difficulty in fact, that they twist anot"\
+					<< "her leg." << std::endl;
 				this->BreakALeg();
 			}
 		}
 		else
 			std::cout << name << " walks happyly toward you." << std::endl;
+		return true;
+	}
+}
+
+bool	Pony::Run() {
+	if (!this->isAlive)
+		return false;
+	else {
+		if (!this->isAwake) {
+			std::cout << name << " starts sleepwalking, but faster than usual."\
+				<< std::endl;
+			if (!(rand() % 2)){
+				std::cout << "So fast, and so blindly, that they run into a wa"\
+					<< "ll, and break one of their leg." << std::endl;
+				this->BreakALeg();
+			}
+		} 
+		else {
+			if (!this->legsBroken)
+				std::cout << name << " runs valiantly across the field." \
+					<< std::endl;
+			else
+				std::cout << name << " walks as fast as their broken legs allo"\
+					<< "w them to." << std::endl;
+			if (!(rand() % 2)){
+				std::cout << "The effort required for this prowess proves too "\
+					<< "much for " << name << ", and so, they inadvertantly tw"\
+					<< "ist a leg." << std::endl;
+				this->BreakALeg();
+			}
+		}
 		return true;
 	}
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/18 19:09:35 by abaur             #+#    #+#             */
-/*   Updated: 2021/03/18 21:30:54 by abaur            ###   ########.fr       */
+/*   Updated: 2021/03/18 21:57:07 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,16 @@
 static void	PonyAdventuresTwoPointO(Pony *pony){
 	(void)pony;
 
-	pony->Sleep();
-	pony->Sleep();
 	pony->WakeUp();
 	pony->Jump();
 	pony->Walk();
+	pony->Run();
+
 	pony->Sleep();
+	pony->Sleep();
+	pony->Jump();
+	pony->Walk();
+	pony->Run();
 }
 
 static void	PonyOnTheHeap(){
