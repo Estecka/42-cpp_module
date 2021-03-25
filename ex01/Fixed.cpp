5e5aad451764f668fd961a435784d8ee010ad35e
abaur <abaur@student.42.fr>
2021-03-25 15:54:20 +0100

    ex01 Basic implementations.

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 19:24:30 by abaur             #+#    #+#             */
-/*   Updated: 2021/03/24 19:46:53 by abaur            ###   ########.fr       */
+/*   Updated: 2021/03/25 15:50:05 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,24 @@ Fixed::Fixed(const Fixed& original){
 	std::cout << "Copy constructor called" << std::endl;
 	*this = original;
 }
+Fixed::Fixed(int value){
+	this->raw = value << fixedPoint;
+}
+Fixed::Fixed(float value){
+	this->raw = (int)(value * (1 << fixedPoint));
+}
 Fixed::~Fixed(){
 	std::cout << "Destructor called" << std::endl;
 }
+
 
 Fixed& Fixed::operator =(const Fixed& original){
 	std::cout << "Assignation operator called" << std::endl;
 	this->raw = original.getRawBits();
 	return *this;
+}
+std::ostream& operator <<(std::ostream& dst, const Fixed& src){
+	return dst << src.toString();
 }
 
 
@@ -43,7 +53,47 @@ int 	Fixed::getRawBits() const {
 	std::cout << "getRawBits member function called" << std::endl;
 	return this->raw; 
 }
-void	Fixed::setRawBits(int raw){
+void	Fixed::setRawBits(const int raw){
 	std::cout << "setRawBits member function called" << std::endl;
 	this->raw = raw;
+}
+
+
+int	Fixed::toInt() const {
+	return this->raw >> fixedPoint;
+}
+float	Fixed::toFloat() const {
+	return ((float)this->raw) / (1 << fixedPoint);
+}
+
+std::string	Fixed::toString() const{
+	std::string	result = "";
+	int 	decimalPoint = fixedPoint;
+	long	rawl = this->raw;
+
+	// Creates an integer with the same digits.
+	for (int i=0; i<fixedPoint; i++)
+		rawl *= 10;
+	rawl >>= fixedPoint;
+	// Removes trailing 0 decimals
+	/* while (decimalPoint > 0 &&  !(rawl % 10)){
+	// 	rawl /= 10;
+	// 	decimalPoint --;
+	// } */
+	// Write the fractional portion
+	for (int i=0; i<decimalPoint; i++){
+		char c = '0' + (rawl % 10);
+		result.insert(0, &c, 1);
+		rawl /= 10;
+	}
+	if (decimalPoint)
+		result.insert(0, ".");
+	// Write the integral portion
+	// do-while ensures at least one digit is printed.
+	do {
+		char c = '0' + (rawl % 10);
+		result.insert(0, &c, 1);
+		rawl /= 10;
+	} while (rawl);
+	return result;
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,18 +6,22 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 19:08:33 by abaur             #+#    #+#             */
-/*   Updated: 2021/03/24 19:36:48 by abaur            ###   ########.fr       */
+/*   Updated: 2021/03/25 14:36:10 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed
 {
 public:
 	Fixed(void);
 	Fixed(const Fixed& original);
+	Fixed(int value);
+	Fixed(float value);
 	~Fixed();
 
 	Fixed& operator =(const Fixed& original);
@@ -27,9 +31,15 @@ public:
 	int 	getRawBits() const;
 	void	setRawBits(const int raw);
 
+	int  	toInt() const;
+	float	toFloat() const;
+	std::string	toString() const;
+
 private:
 	static const int	fixedPoint;
 	signed int	raw;
 };
+
+std::ostream& operator <<(std::ostream& dst, const Fixed& src);
 
 #endif
