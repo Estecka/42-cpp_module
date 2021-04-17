add4df3642b25436e41fb67073cad41a25354678
abaur <abaur@student.42.fr>
2021-04-17 18:57:38 +0200

    ex02 Implementations

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 18:28:25 by abaur             #+#    #+#             */
-/*   Updated: 2021/04/17 18:40:03 by abaur            ###   ########.fr       */
+/*   Updated: 2021/04/17 18:52:33 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,47 @@
 #define ARRAY_HPP
 
 #include <stdlib.h>
+#include <stdexcept>
+#include <sstream>
 
 template <typename T>
 class Array
 {
 public:
-	Array(void);
-	Array(const Array<T>&);
-	Array(int size);
-	~Array();
-	Array<T>	operator=(const Array<T>&);
+	Array(void) {
+		this->content = NULL;
+		this->length = 0;
+	}
+	Array(const Array<T>& other){
+		*this = other;
+	}
+	Array(int size) {
+		this->content = new T[size];
+		this->length = size;
+	}
+	~Array() {
+		if (this->length)
+			delete[] this->content;
+	}
+	Array<T>	operator=(const Array<T>& other) {
+		this->length = other.length;
+		this->content = new T[other.length];
+		for (size_t i=0; i<length; i++)
+			this[i] = other[i];
+	}
 
-	T&	operator[](size_t);
-	size_t	size() const;
+	T&	operator[](size_t index) {
+		if (0 <= index && index < length)
+			return this->content[index];
+		else {
+			std::ostringstream	what;
+			what << "Index "<<index<<" out of "<<length <<" is invalid.";
+			throw std::out_of_range(what.str());
+		}
+	}
+	size_t	size() const {
+		return this->length;
+	}
 
 private:
 	T*	content;
