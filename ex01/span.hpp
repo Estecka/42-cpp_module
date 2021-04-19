3fa798dd119250a0b3ad2687e72d95b2dd0cc3cc
abaur <abaur@student.42.fr>
2021-04-19 18:04:58 +0200

    ex01 Init

---
+++ ex01/span.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/19 17:56:28 by abaur             #+#    #+#             */
+/*   Updated: 2021/04/19 18:03:58 by abaur            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <stdlib.h>
+
+class Span
+{
+public:
+	Span(void);
+	Span(size_t capacity);
+	Span(const Span&);
+
+	Span&	operator=(const Span&);
+	int&	operator[](size_t index);
+
+	void	addNumber(int n);
+	int	shortestSpan() const;
+	int	longestSpan () const;
+
+private:
+	int*	content;
+	size_t	capacity;
+	size_t	length;
+};
+
+#endif
