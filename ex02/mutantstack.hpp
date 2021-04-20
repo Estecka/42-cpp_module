184aaea0e339f564120f1da0c6efc637c927b0ef
abaur <abaur@student.42.fr>
2021-04-20 20:07:00 +0200

    ex02 Stack implementation.

--- ex02/mutantstack.hpp
+++ ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 18:56:35 by abaur             #+#    #+#             */
-/*   Updated: 2021/04/20 19:43:43 by abaur            ###   ########.fr       */
+/*   Updated: 2021/04/20 20:03:10 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ template <typename T, class Container = std::deque<T> >
 class mutantstack
 {
 public:
-	mutantstack(void);
-	mutantstack(const mutantstack&);
-	~mutantstack();
+	mutantstack(void) : _stack() {}
+	mutantstack(const mutantstack& other) : _stack(other._stack) {}
+	~mutantstack() {}
 
-	mutantstack&	operator=(const mutantstack&);
+	mutantstack&	operator=(const mutantstack& other) { _stack = other._stack; }
 
 	// All c++11 extension; cannot be implemented
 	//using container_type  = typename std::stack<T>::container_type;
@@ -38,26 +38,25 @@ public:
 	//using refernce        = typename std::stack<T>::reference;
 	//using const_reference = typename std::stack<T>::const_reference;
 
-	Treference	top();
-	Tconst_reference	top() const;
+	Treference      	top()       { return _stack.top(); }
+	Tconst_reference	top() const { return _stack.top(); }
 
-	bool	empty() const;
+	bool	empty() const { return _stack.empty(); }
 
-	Tsize_type	size() const;
+	Tsize_type	size() const { return _stack.size(); }
 
-	void	push(const Tvalue_type& value);
-	void	pop();
+	void	push(const Tvalue_type& value) { return _stack.push(value); }
+	void	pop() { _stack.pop(); }
 
-	bool	operator==(const mutantstack& rhs) const;
-	bool	operator!=(const mutantstack& rhs) const;
-	bool	operator<=(const mutantstack& rhs) const;
-	bool	operator>=(const mutantstack& rhs) const;
-	bool	operator<(const mutantstack& rhs) const;
-	bool	operator>(const mutantstack& rhs) const;
+	bool	operator==(const mutantstack& rhs) const { return _stack == rhs._stack; }
+	bool	operator!=(const mutantstack& rhs) const { return _stack != rhs._stack; }
+	bool	operator<=(const mutantstack& rhs) const { return _stack <= rhs._stack; }
+	bool	operator>=(const mutantstack& rhs) const { return _stack >= rhs._stack; }
+	bool	operator< (const mutantstack& rhs) const { return _stack <  rhs._stack; }
+	bool	operator> (const mutantstack& rhs) const { return _stack >  rhs._stack; }
 
 private:
 	std::stack<T, Container>	_stack;
-	const std::stack<T, Container>*const	_cstack;
 };
 
 #endif
