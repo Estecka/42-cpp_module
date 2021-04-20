1fb4d6634332d887f44d852e7e386c4d74414747
abaur <abaur@student.42.fr>
2021-04-20 21:17:41 +0200

    ex02 Dump

--- ex02/mutantstack.hpp
+++ ex02/mutantstack.hpp
@@ -6,25 +6,26 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 18:56:35 by abaur             #+#    #+#             */
-/*   Updated: 2021/04/20 21:12:41 by abaur            ###   ########.fr       */
+/*   Updated: 2021/04/20 21:17:30 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <iostream>
 #include <stack>
 
-template <typename T, class Container = std::deque<T> >
-class MutantStack : public std::stack<T, Container>
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 public:
-	MutantStack(void) : std::stack<T, Container>() {}
-	MutantStack(const std::stack<T, Container>& other) : std::stack<T, Container>(other) {}
+	MutantStack(void) : std::stack<T>() {}
+	MutantStack(const std::stack<T>& other) : std::stack<T>(other) {}
 	~MutantStack() {}
 
-	MutantStack&	operator=(const std::stack<T, Container>& other) {
-		this->std::stack<T, Container>::operator=(other);
+	MutantStack&	operator=(const std::stack<T>& other) {
+		this->std::stack<T>::operator=(other);
 		return *this;
 	}
 
@@ -47,6 +48,17 @@ public:
 
 	iterator	begin() { return iterator(); }
 	iterator	end()   { return iterator(); }
+
+	void	Dump() const {
+		std::cout << "["<<this->size()<<"]{ ";
+		for (int i=0; i<this->size(); i++)
+		{
+			std::cout << this->c[i];
+			if (i+1 < this->size)
+				std::cout << ", ";
+		}
+		std::cout << " }";
+	}
 };
 
 #endif
